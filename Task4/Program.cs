e66ed36905e26bba1f4fe62501fbee190c144a05
ElenaPreobraj <romanovae@agrosemcenter.ru>
2022-12-18 22:56:21 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1000 && num > 99)
+{
+    int result = num % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нельзя такое число!");
+}
