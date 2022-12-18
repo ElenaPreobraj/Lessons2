12234bd57a7befbd654795e84edca5e858a7c4e4
ElenaPreobraj <romanovae@agrosemcenter.ru>
2022-12-18 22:49:19 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+/*int num = -n;
+while (num<=n)
+{
+    Console.WriteLine(num);
+    Console.Write(", ");
+    num++;
+}*/
+
+for (int i = -n; i <=n; i++)
+{
+    Console.Write(i);
+    Console.Write(", ");
+}
