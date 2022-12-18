da3c1ab111411265dae78917d49bbf42b6f868bb
ElenaPreobraj <romanovae@agrosemcenter.ru>
2022-12-18 21:53:31 +0300

    Add 0 task

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
